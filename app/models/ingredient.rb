8b7f3ea5bc02360be54f2cd1476ff7b1b9aa8c64
Raghu Betina <raghu@firstdraft.com>
2017-03-12 22:27:19 +0000

    Direct association between ingredients and recipe_ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,9 @@
 class Ingredient < ApplicationRecord
   # Direct associations
 
+  has_many   :recipe_ingredients,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,6 +1,8 @@
 class RecipeIngredient < ApplicationRecord
   # Direct associations
 
+  belongs_to :ingredient
+
   belongs_to :recipe
 
   # Indirect associations
