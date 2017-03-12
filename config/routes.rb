72b6aac70f680cd36abf0123801fb938322133fe
Raghu Betina <raghu@firstdraft.com>
2017-03-12 22:26:49 +0000

    Generate recipes resource with starter:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the Recipe resource:
+  # CREATE
+  get "/recipes/new", :controller => "recipes", :action => "new"
+  post "/create_recipe", :controller => "recipes", :action => "create"
+
+  # READ
+  get "/recipes", :controller => "recipes", :action => "index"
+  get "/recipes/:id", :controller => "recipes", :action => "show"
+
+  # UPDATE
+  get "/recipes/:id/edit", :controller => "recipes", :action => "edit"
+  post "/update_recipe/:id", :controller => "recipes", :action => "update"
+
+  # DELETE
+  get "/delete_recipe/:id", :controller => "recipes", :action => "destroy"
+  #------------------------------
+
   # Routes for the Meal resource:
   # CREATE
   get "/meals/new", :controller => "meals", :action => "new"
