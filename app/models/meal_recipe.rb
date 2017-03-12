f192be2f30727a8f413436fd825b8db2bb0887b2
Raghu Betina <raghu@firstdraft.com>
2017-03-12 22:27:19 +0000

    Direct association between recipes and meal_recipes

--- app/models/meal_recipe.rb
+++ app/models/meal_recipe.rb
@@ -1,6 +1,8 @@
 class MealRecipe < ApplicationRecord
   # Direct associations
 
+  belongs_to :recipe
+
   belongs_to :meal
 
   # Indirect associations
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,9 @@
 class Recipe < ApplicationRecord
   # Direct associations
 
+  has_many   :meal_recipes,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
