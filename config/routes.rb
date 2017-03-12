309eb36a5ba10be5d7d6fdc73f309e338d3f3d9d
Raghu Betina <raghu@firstdraft.com>
2017-03-12 22:26:47 +0000

    Generate meals resource with starter:resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  # Routes for the Meal resource:
+  # CREATE
+  get "/meals/new", :controller => "meals", :action => "new"
+  post "/create_meal", :controller => "meals", :action => "create"
+
+  # READ
+  get "/meals", :controller => "meals", :action => "index"
+  get "/meals/:id", :controller => "meals", :action => "show"
+
+  # UPDATE
+  get "/meals/:id/edit", :controller => "meals", :action => "edit"
+  post "/update_meal/:id", :controller => "meals", :action => "update"
+
+  # DELETE
+  get "/delete_meal/:id", :controller => "meals", :action => "destroy"
+  #------------------------------
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
