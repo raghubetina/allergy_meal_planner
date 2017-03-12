8c1124fee9249d6d350e54295ad1f9d73f1ad9a8
Raghu Betina <raghu@firstdraft.com>
2017-03-12 22:27:33 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "meals#index"
   # Routes for the Cookbook_meal resource:
   # CREATE
