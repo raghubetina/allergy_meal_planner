2c1e95ade66f7c67ae89914856fe8db00ad01a21
Raghu Betina <raghu@firstdraft.com>
2017-03-12 22:28:10 +0000

    Whitelist columns in CookbookMeal for ActiveAdmin

--- app/admin/cookbook_meal.rb
+++ app/admin/cookbook_meal.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register CookbookMeal do
 
+ permit_params :cookbook_id, :meal_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
